66ad1fceac4bdbd38f971bf3de2fc991c030c0fe
nhmthanh-0210 <thanhnguyenminhho2003@gmail.com>
2024-09-25 22:22:26 +0700

    update title

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Bảng Cửu Chương Nằm Ngang</title>
+    <title>Bảng Cửu Chương</title>
     <style>
         * {
             margin: 0;
